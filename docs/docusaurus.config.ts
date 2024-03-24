bb9901df9c62b044f2efeea8c27dcee317b38292
Jean Flesch <jean.flesch93@gmail.com>
2024-03-24 20:00:55 -0300

    gha: docusaurus

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
+  title: 'TS PokéAPI',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -21,6 +21,8 @@ const config: Config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
+  deploymentBranch: 'gh-pages',
+  trailingSlash: true,
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
