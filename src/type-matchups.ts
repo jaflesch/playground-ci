44f701a8c17587f7bccb5cd6647b540fa124c369
Jean Flesch <jean.flesch93@gmail.com>
2024-04-21 23:17:31 -0300

    wip: types matchup

---
+++ src/type-matchups.ts
@@ -0,0 +1,101 @@
+export enum TypesIndex {
+  NORMAL,
+  FIRE,
+  WATER,
+  ELECTRIC,
+  GRASS,
+  ICE,
+  FIGHTING,
+  POISON,
+  GROUND,
+  FLYING,
+  PSYCHIC,
+  BUG,
+  ROCK,
+  GHOST,
+  DRAGON,
+  DARK,
+  STEEL,
+  FAIRY,
+}
+
+export const TYPES_MATCHUP_MATRIX = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0, 1, 1, 0.5, 1], // Normal
+  [1, 0.5, 0.5, 1, 2, 2, 1, 1, 1, 1, 1, 2, 0.5, 1, 0.5, 1, 2, 1], // Fire
+  [1, 2, 0.5, 1, 0.5, 1, 1, 1, 2, 1, 1, 1, 2, 1, 0.5, 1, 1, 1], // Water
+  [1, 1, 2, 0.5, 0.5, 1, 1, 1, 0, 2, 1, 1, 1, 1, 0.5, 1, 1, 1], // Electric
+  [1, 0.5, 2, 1, 0.5, 1, 1, 0.5, 2, 0.5, 1, 0.5, 2, 1, 0.5, 1, 0.5, 1], // Grass
+  [1, 0.5, 0.5, 1, 2, 0.5, 1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 0.5, 1], // Ice
+  [2, 1, 1, 1, 1, 2, 1, 0.5, 1, 0.5, 0.5, 0.5, 2, 0, 1, 2, 2, 0.5], // Fighting
+  [1, 1, 1, 1, 2, 1, 1, 0.5, 0.5, 1, 1, 1, 0.5, 0.5, 1, 1, 0, 2], // Poison
+  [1, 2, 1, 2, 0.5, 1, 1, 2, 1, 0, 1, 0.5, 2, 1, 1, 1, 2, 1], // Ground
+  [1, 1, 1, 0.5, 2, 1, 2, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 0.5, 1], // Flying
+  [1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 0.5, 1, 1, 1, 1, 0, 0.5, 1], // Psychic
+  [1, 0.5, 1, 1, 2, 1, 0.5, 0.5, 1, 0.5, 2, 1, 1, 0.5, 1, 2, 0.5, 0.5], // Bug
+  [1, 2, 1, 1, 1, 2, 0.5, 1, 0.5, 2, 1, 2, 1, 1, 1, 1, 0.5, 1], // Rock
+  [0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 0.5, 1, 1], // Ghost
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 0.5, 0], // Dragon
+  [1, 1, 1, 1, 1, 1, 0.5, 1, 1, 1, 2, 1, 1, 2, 1, 0.5, 1, 0.5], // Dark
+  [1, 0.5, 0.5, 0.5, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 0.5, 2], // Steel
+  [1, 0.5, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 1, 1, 1, 2, 2, 0.5, 1], // Fairy
+];
+
+export const getOffensiveMatchups = (index: TypesIndex) => {
+  const normal = [];
+  const noEffect = [];
+  const superEffective = [];
+  const notEffective = [];
+
+  for (let i = 0; i < TYPES_MATCHUP_MATRIX[index].length; i++) {
+    switch (TYPES_MATCHUP_MATRIX[index][i]) {
+      case 0:
+        noEffect.push(i);
+        break;
+      case 1:
+        normal.push(i);
+        break;
+      case 0.5:
+        notEffective.push(i);
+        break;
+      case 2:
+        superEffective.push(i);
+        break;
+    }
+  }
+  return {
+    normal,
+    noEffect,
+    notEffective,
+    superEffective,
+  };
+};
+
+export const getDefensiveMatchups = (index: TypesIndex) => {
+  const normal = [];
+  const noEffect = [];
+  const superEffective = [];
+  const notEffective = [];
+
+  for (let i = 0; i < TYPES_MATCHUP_MATRIX.length; i++) {
+    switch (TYPES_MATCHUP_MATRIX[i][index]) {
+      case 0:
+        noEffect.push(i);
+        break;
+      case 1:
+        normal.push(i);
+        break;
+      case 0.5:
+        notEffective.push(i);
+        break;
+      case 2:
+        superEffective.push(i);
+        break;
+    }
+  }
+  return {
+    normal,
+    noEffect,
+    notEffective,
+    superEffective,
+  };
+};
